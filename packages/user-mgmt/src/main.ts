d2af4772747bc6c365a4e520bd131f782870ef67
Trung Thành Phạm <adamward459@gmail.com>
2025-01-28 22:03:44 +0700

    feat: add package core

--- packages/user-mgmt/src/main.ts
+++ packages/user-mgmt/src/main.ts
@@ -16,6 +16,7 @@ app.get('/api', (req, res) => {
 });
 
 const port = process.env.PORT || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
